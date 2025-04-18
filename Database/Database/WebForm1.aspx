f7d8feedefdca93adfc300705907c48e6c8ca1db
Vinayak-Chv <bcavinayakchavan@gmail.com>
2025-04-18 12:41:53 +0530

    Added the Gridview Components and Done coding part of the Backend Only CSS is Remaining

--- Database/Database/WebForm1.aspx
+++ Database/Database/WebForm1.aspx
@@ -88,26 +88,38 @@
                 </tr>
                 <tr>
                     <td class="auto-style1">
-                        <asp:Button ID="Button1" runat="server" Text="Register" />
+                        <asp:Button ID="Button1" runat="server" Text="Register" OnClick="Button1_Click" />
                     </td>
                     <td class="auto-style4">
-                        <asp:Button ID="Button2" runat="server" Text="Update" />
+                        <asp:Button ID="Button2" runat="server" Text="Update" OnClick="Button2_Click" />
                     </td>
                 </tr>
                 <tr>
                     <td class="auto-style1">
-                        <asp:Button ID="Button3" runat="server" Text="Delete" />
+                        <asp:Button ID="Button3" runat="server" Text="Delete" OnClick="Button3_Click" />
                     </td>
                     <td class="auto-style4">
-                        <asp:Button ID="Button4" runat="server" Text="Display" />
+                        <asp:Button ID="Button4" runat="server" Text="Display" OnClick="Button4_Click" />
                     </td>
                 </tr>
             </table>
 
 
             <br />
-            <asp:GridView ID="GridView1" runat="server">
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="Account_Id" DataSourceID="SqlDataSource1">
+                <Columns>
+                    <asp:BoundField DataField="Account_Id" HeaderText="Account_Id" ReadOnly="True" SortExpression="Account_Id" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                    <asp:BoundField DataField="Age" HeaderText="Age" SortExpression="Age" />
+                    <asp:BoundField DataField="Address" HeaderText="Address" SortExpression="Address" />
+                    <asp:BoundField DataField="Email_Id" HeaderText="Email_Id" SortExpression="Email_Id" />
+                    <asp:BoundField DataField="Account_Type" HeaderText="Account_Type" SortExpression="Account_Type" />
+                    <asp:BoundField DataField="Contact_No" HeaderText="Contact_No" SortExpression="Contact_No" />
+                </Columns>
             </asp:GridView>
+
+
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" ProviderName="<%$ ConnectionStrings:ConnectionString.ProviderName %>" SelectCommand="SELECT * FROM [Customer]"></asp:SqlDataSource>
 
 
         </div>
--- Database/Database/WebForm1.aspx.cs
+++ Database/Database/WebForm1.aspx.cs
@@ -4,14 +4,79 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace Database
 {
 	public partial class WebForm1 : System.Web.UI.Page
 	{
+		string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\admin\Desktop\Database Project\Database\Database\App_Data\Database1.mdf"";Integrated Security=True";
+
 		protected void Page_Load(object sender, EventArgs e)
 		{
-
+            if (!IsPostBack)
+            {
+                GridView1.DataBind();
+            }
 		}
-	}
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+			using (SqlConnection conn = new SqlConnection(connStr))
+			{
+				string query = "INSERT INTO Customer (Account_Id ,Name, Age, Address, Email_Id, Account_Type, Contact_No) VALUES (@Account_Id ,@Name, @Age, @Address, @Email_Id, @Account_Type, @Contact_No)";
+				SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Account_Id", int.Parse(TextBox1.Text));
+                cmd.Parameters.AddWithValue("@Name", TextBox2.Text);
+				cmd.Parameters.AddWithValue("@Age", int.Parse(TextBox3.Text));
+				cmd.Parameters.AddWithValue("@Address", TextBox4.Text);
+				cmd.Parameters.AddWithValue("@Email_Id", TextBox5.Text);
+				cmd.Parameters.AddWithValue("@Account_Type", DropDownList1.SelectedValue);
+				cmd.Parameters.AddWithValue("@Contact_No", int.Parse(TextBox6.Text));
+				conn.Open();
+				cmd.ExecuteNonQuery();
+				conn.Close();
+			}
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "UPDATE Customer SET Name=@Name, Age=@Age, Address=@Address, Email_Id=@Email_Id, Account_Type=@Account_Type, Contact_No=@Contact_No WHERE Account_Id=@Account_Id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Account_Id", int.Parse(TextBox1.Text));
+                cmd.Parameters.AddWithValue("@Name", TextBox2.Text);
+                cmd.Parameters.AddWithValue("@Age", int.Parse(TextBox3.Text));
+                cmd.Parameters.AddWithValue("@Address", TextBox4.Text);
+                cmd.Parameters.AddWithValue("@Email_Id", TextBox5.Text);
+                cmd.Parameters.AddWithValue("@Account_Type", DropDownList1.SelectedValue);
+                cmd.Parameters.AddWithValue("@Contact_No", int.Parse(TextBox6.Text));
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "DELETE FROM Customer WHERE Account_Id=@Account_Id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Account_Id", int.Parse(TextBox1.Text));
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                GridView1.DataBind();
+            }
+        }
+    }
 }
